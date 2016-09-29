5f67cddd670509a11a18d1eeb870e6499ee8c90a
Alyssa Kwan <me@alyssackwan.name>
2016-09-29 03:06:07 +0000

    fix root controller

--- tf_idf/app/controllers/root_controller.py
+++ tf_idf/app/controllers/root_controller.py
@@ -4,5 +4,6 @@ class RootController(object):
         return '''
             <ul>
                 <li><a href="/calculations">Exercise #1: Calculations</a></li>
+                <li><a href="/search">Exercise #2: Search</a></li>
             </ul>
         '''
