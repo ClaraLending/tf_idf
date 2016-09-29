0b19cafdc1e1bce27bcd3eec34b29e38ebb253d9
Alyssa Kwan <me@alyssackwan.name>
2016-09-29 03:04:45 +0000

    search engine solution

--- tf_idf/app/models/search_engine.py
+++ tf_idf/app/models/search_engine.py
@@ -4,9 +4,15 @@ class SearchEngine(object):
         self._corpus = corpus
         
     def search_by_word(self, word, offset=0, count=25):
-        results = []
-
         # @TODO: implement the search algorithm
         # put the results into the results list
-
+        if word not in self._corpus.words:
+            return []
+        document_scores = []
+        for document_title, document in self._corpus.documents.items():
+            score = self._corpus.words[word].tf_idf_linear_boolean(document_title)
+            if score > 0.000001:
+                document_scores.append((score, document))
+        document_scores = sorted(document_scores, key=lambda x: x[0])
+        results = [x[1] for x in document_scores]
         return results
